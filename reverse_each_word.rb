ebc230816f80dc64105d0bcbefa92d9a0f6198d8
Chris Gullickson <cgullickson5@gmail.com>
2017-11-21 21:25:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-def reverse_each_word (string)
-  arr = string.split(" ")
-  message = []
-  arr.each do |word|
-    message << word.reverse
-  end
-  message.join(" ")
-end
+
 
 def reverse_each_word
   arr = string.split(" ")
@@ -13,3 +6,4 @@ def reverse_each_word
     word.reverse
   end
   arr.join(" ")
+end
